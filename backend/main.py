de71629f2497e000b5c41bc90a82bdaf24dd00b6
rickyruozzi <ruozziriccardo4@gmail.com>
2025-03-16 10:10:07 +0100

    inserita homepage

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pymongo
 
 app = FastAPI()
+templates=Jinja2Templates(directory='templates')
+
 
 # Add CORS middleware
 app.add_middleware(
@@ -28,8 +34,14 @@ def create_connection():
 async def submit_entry(diary_entry: DiaryEntry):
     collection=create_connection()
     collection.insert_one(diary_entry.dict())
-
     return {"message": "Diary entry received", "entry": diary_entry.entry, "date": diary_entry.date}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request:Request):
+    with open("backend/templates/index.html") as f: #apre il file della pagina da mostrare
+        content = f.read() #ne legge il contenuto
+    return HTMLResponse(content)    #restituisce in output la pagina renderizzata
+
 
 if __name__ == "__main__":
     import uvicorn
